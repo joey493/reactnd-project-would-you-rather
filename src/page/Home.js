90547fdac1fbaaec3969772f584c8eb2679fcdbb
joey493 <ebrahimnader321@gmail.com>
2021-12-18 01:43:30 +0200

    update Home & NewQ to hooks

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react'
-import { Switch, Route, NavLink } from 'react-router-dom'
+import { Switch, Route, NavLink, useRouteMatch } from 'react-router-dom'
 import Answered from '../component/Answered'
 import Unanswered from '../component/Unanswered'
 
-export class Home extends Component {
-    render() {
-        const { match } = this.props
+const Home =  () => {
+    const { url } = useRouteMatch()
 
-        return (
-            <main className="main">
-                <nav className='flex  overflow-hidden'>
-                    <NavLink activeClassName='bg-main-color text-white' className='home-nav' to='/home/unanswered'>Unanswered</NavLink>
-                    <NavLink activeClassName='bg-main-color text-white' className='home-nav' to='/home/answered'>Answered</NavLink>
-                </nav>
-                <Switch>
-                    <Route exact path={`${match.url}/unanswered`} component={Unanswered} />
-                    <Route exact path={`${match.url}/answered`} component={Answered} />
-                </Switch>
-            </main>
-        )
-    }
+    return (
+        <main className="main">
+            <nav className='flex  overflow-hidden'>
+                <NavLink activeClassName='bg-main-color text-white' className='home-nav' to='/home/unanswered'>Unanswered</NavLink>
+                <NavLink activeClassName='bg-main-color text-white' className='home-nav' to='/home/answered'>Answered</NavLink>
+            </nav>
+            <Switch>
+                <Route exact path={`${url}/unanswered`} component={Unanswered} />
+                <Route exact path={`${url}/answered`} component={Answered} />
+            </Switch>
+        </main>
+    )
 }
 
 export default Home
--- src/page/NewQ.js
+++ src/page/NewQ.js
@@ -1,49 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { handleSaveQuestion } from '../redux/action/questions'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { handleSaveQuestion } from '../redux/action/questions'
 
-export class NewQ extends Component {
-    state = {
-        optionOneText: '',
-        optionTwoText: '',
-        toHome: false
+const NewQ = () => {
+    const [ optionOneText, setOptionOne ] = useState('')
+    const [ optionTwoText, setOptionTwo ] = useState('')
+    const [ toHome, setToHome ] = useState(false)
+    const dispatch = useDispatch()
+    const currentUser = useSelector(state => state.currentUser)
+
+    const handleOption = (e, option) => {
+        if (option === 'one') setOptionOne(e.target.value);
+        if (option === 'two') setOptionTwo(e.target.value);
     }
 
-    handleOptionOne = (e) => this.setState({ optionOneText: e.target.value })
-    handleOptionTwo = (e) => this.setState({ optionTwoText: e.target.value })
-
-    handleSubmit = () => {
-        const { optionOneText, optionTwoText } = this.state
-        const { dispatch, currentUser } = this.props
-
+    const handleSubmit = () => {
         dispatch(handleSaveQuestion({ optionOneText, optionTwoText, author: currentUser.id }))
 
-        this.setState({
-            toHome: true
-        })
+        setToHome({ toHome: true })
     }
 
-    render() {
-        const { optionOneText, optionTwoText, toHome } = this.state
-
-        if (toHome === true) {
-            return <Redirect to='/home' />
-        }
-
-        return (
-            <div className='main p-8 flex flex-col items-center'>
+    return (
+        <>{toHome 
+            ? <Redirect to='/home' />
+            :<div className='main p-8 flex flex-col items-center'>
                 <h2 className='font-bold text-lg mb-8'>Would You Rather...</h2>
                 {/* options */}
-                <input className='set-border w-full px-4 py-2' placeholder='Option 1' onChange={this.handleOptionOne} />
+                <input className='set-border w-full px-4 py-2' placeholder='Option 1' onChange={e => handleOption(e, 'one')} />
                 <p className='font-bold text-lg my-6'>Or</p>
-                <input className='set-border w-full px-4 py-2' placeholder='Option 2' onChange={this.handleOptionTwo} />
-                <button className='mt-6 mx-auto btn' onClick={this.handleSubmit} disabled={optionOneText === '' || optionTwoText === ''}>Submit</button>
+                <input className='set-border w-full px-4 py-2' placeholder='Option 2' onChange={e => handleOption(e, 'two')} />
+                <button className='mt-6 mx-auto btn' onClick={handleSubmit} disabled={optionOneText === '' || optionTwoText === ''}>Submit</button>
             </div>
-        )
-    }
+        }
+        </>
+    )
 }
 
-const mapStateToProps = ({ currentUser },) => ({ currentUser })
-
-export default connect(mapStateToProps)(NewQ)
+export default NewQ
